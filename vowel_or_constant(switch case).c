37abb39f4ef74cd2c2e55d86e91a368b6e55797a
gownirishitha513-sys <gownirishitha513@gmail.com>
2026-03-23 08:15:59 +0530

    Create vowel_or_constant(switch case).c

---
+++ vowel_or_constant(switch case).c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+ char ch;
+ printf("enter character");
+ scanf("%c", &ch);
+ switch(ch){
+ case'a':
+ case'e':
+ case'i':
+ case'o':
+ case'u':
+ case'A':
+ case'E':
+ case'I':
+ case'O':
+ case'U':
+ printf("vowel");
+ break;
+ default:
+ printf("constant");
+ }
+ return 0;
+ }
+ 
