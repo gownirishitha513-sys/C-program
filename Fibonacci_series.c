e86f36c57ed2e928156d66f7595f116d009f8269
gownirishitha513-sys <gownirishitha513@gmail.com>
2026-03-29 08:47:37 +0530

    Create Fibonacci_series.c

---
+++ Fibonacci_series.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int n,i;
+int a=0,b=1,next;
+printf("enter number");
+scanf("%d",&n);
+printf("Fibonacci series:");
+for(i=1;i<=n;i++){
+printf("%d ", a);
+next=a+b;
+a=b;
+b=next;
+}
+return 0;
+}
