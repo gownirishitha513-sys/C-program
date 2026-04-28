939a5a280eed7738535c02185a0a969b12123519
gownirishitha513-sys <gownirishitha513@gmail.com>
2026-04-28 23:41:01 +0530

    Create union_of_arrays.c

---
+++ union_of_arrays.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+int arr1[100],arr2[100],UnionArr[200],n1,n2,i,j,k=0,found;
+printf("enter size of first array");
+scanf("%d",&n1);
+printf("enter elements of first array");
+for(i=0;i<n1;i++){
+scanf("%d",&arr1[i]);
+UnionArr[k++]=arr1[i];
+}
+printf("enter size of second array");
+scanf("%d",&n2);
+printf("enter elements of second array");
+for(i=0;i<n2;i++){
+scanf("%d",&arr2[i]);
+}
+for(i=0;i<n2;i++){
+found=0;
+for(j=0;j<k;j++){
+if(arr2[i]==UnionArr[j]){
+found=1;
+break;
+}}
+if(found==0){
+UnionArr[k++]=arr2[i];
+}
+}
+printf("Union of arrays;\n");
+for(i=0;i<k;i++){
+printf("%d ", UnionArr[i]);
+}
+return 0;
+}
