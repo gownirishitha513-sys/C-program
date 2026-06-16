3fcfe3e25e291ab08d38ef3e5aeb090ea3b933e7
gownirishitha513-sys <gownirishitha513@gmail.com>
2026-06-16 22:21:57 +0530

    Create find_minimum_of_two_numbers_using_pointers.c

---
+++ find_minimum_of_two_numbers_using_pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int a, b;
+int *p1, *p2;
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+p1 = &a;
+p2 = &b;
+if (*p1 < *p2)
+printf("Minimum = %d\n", *p1);
+else
+printf("Minimum = %d\n", *p2);
+return 0;
+}
