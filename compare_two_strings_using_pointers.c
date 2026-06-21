c9506a255c4a681021e81f1a3629c0957580c921
gownirishitha513-sys <gownirishitha513@gmail.com>
2026-06-21 10:05:56 +0530

    Create compare_two_strings_using_pointers.c

---
+++ compare_two_strings_using_pointers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+char str1[100], str2[100];
+char *p1, *p2;
+printf("Enter first string: ");
+fgets(str1, sizeof(str1), stdin);
+printf("Enter second string: ");
+fgets(str2, sizeof(str2), stdin);
+p1 = str1;
+p2 = str2;
+while (*p1 == *p2 && *p1 != '\0') {
+p1++;
+p2++;
+}
+if (*p1 == *p2)
+printf("Strings are equal\n");
+else
+printf("Strings are not equal\n");
+return 0;
+}
