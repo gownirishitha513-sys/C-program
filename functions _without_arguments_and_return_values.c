a8ba84d2c4de44820792ef78e5954e261f40060c
gownirishitha513-sys <gownirishitha513@gmail.com>
2026-07-12 09:17:59 +0530

    Create functions _without_arguments_and_return_values.c

---
+++ functions _without_arguments_and_return_values.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void add();   
+int main()
+{
+add();    
+return 0;
+}
+void add(){  
+int a, b, sum;
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+sum = a + b;
+printf("Sum = %d\n", sum);
+}
